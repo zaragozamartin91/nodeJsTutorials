10885cc41e86ef0e48a99c4ad08073b73eb890cf
zaragozamartin91 <zaragozamartin91@gmail.com>
2016-06-14 00:09:20 -0300

    se comenzo a ver algo de angular desde otro tutorial

--- MEAN_Web_Development/Chapter_7/bodyPartExample.js
+++ MEAN_Web_Development/Chapter_7/bodyPartExample.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express(); // create our app w/ express
 var port = process.env.PORT || 8080; // set the port
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -33,7 +33,7 @@ app.post('/api/postding', function(req, res) {
     res.send({
         "test": "data"
     });
-})
+});
 
 
 // listen (start app with node server.js) ======================================
